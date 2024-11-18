0d0aa0459e38448546d575a30287d67b8a73c1a2
FarheenIrfan44 <farheenirfan4040@gmail.com>
2024-11-18 12:14:21 +0500

    updates

--- parser.cpp
+++ parser.cpp
@@ -32,14 +32,13 @@ enum TokenType
     T_RBRACE,
     T_SEMICOLON,
     T_GT,
-    T_LT,   
-    T_EQ,   
-    T_NEQ,  
-    T_AND,  
-    T_OR,   
+    T_LT,
+    T_EQ,
+    T_NEQ,
+    T_AND,
+    T_OR,
     T_WHILE,
     T_FOR,
-    T_AGAR, 
     T_EOF,
 };
 
@@ -47,6 +46,99 @@ struct Token
 {
     TokenType type;
     string value;
+};
+
+struct Symbol
+{
+    TokenType type;
+    int scopeLevel;
+    bool initialized;
+};
+
+class SymbolTable
+{
+private:
+    map<string, Symbol> symbols;
+
+public:
+    void insert(string &name, TokenType type, int scopeLevel)
+    {
+        if (symbols.find(name) == symbols.end())
+        {
+            symbols[name] = {type, scopeLevel, false};
+        }
+        else
+        {
+            cout << "Error: Redefinition of variable '" << name << "'." << endl;
+            exit(1);
+        }
+    }
+
+    bool lookup(const string &name)
+    {
+        return symbols.find(name) != symbols.end();
+    }
+
+    Symbol &get(const string &name)
+    {
+        if (lookup(name))
+        {
+            return symbols[name];
+        }
+        else
+        {
+            cout << "Error: Variable '" << name << "' not declared." << endl;
+            exit(1);
+        }
+    }
+
+    void markInitialized(const string &name)
+    {
+        if (lookup(name))
+        {
+            symbols[name].initialized = true;
+        }
+    }
+    void printTable() const
+    {
+        cout << "Symbol Table:" << endl;
+        cout << "Name\tType\t\tScope\tInitialized" << endl;
+        cout << "--------------------------------------------" << endl;
+        for (const auto &entry : symbols)
+        {
+            const string &name = entry.first;
+            const Symbol &symbol = entry.second;
+
+            // Convert TokenType to a string for display purposes
+            string typeStr;
+            switch (symbol.type)
+            {
+            case T_INT:
+                typeStr = "int";
+                break;
+            case T_FLOAT:
+                typeStr = "float";
+                break;
+            case T_DOUBLE:
+                typeStr = "double";
+                break;
+            case T_STRING:
+                typeStr = "string";
+                break;
+            case T_BOOL:
+                typeStr = "bool";
+                break;
+            case T_CHAR:
+                typeStr = "char";
+                break;
+            default:
+                typeStr = "unknown";
+            }
+
+            cout << name << "\t" << typeStr << "\t\t" << symbol.scopeLevel << "\t"
+                 << (symbol.initialized ? "Yes" : "No") << endl;
+        }
+    }
 };
 
 class Lexer
@@ -81,6 +173,17 @@ public:
                 pos++;
                 continue;
             }
+            // Single line comment
+            if (current == '/' && peek() == '/')
+            {
+                pos += 2;
+                while (src[pos] != '\n')
+                {
+                    pos++;
+                }
+                continue;
+            }
+
             if (isdigit(current))
             {
                 tokens.push_back(Token{T_NUM, consumeNumber()});
@@ -102,8 +205,8 @@ public:
                     tokens.push_back(Token{T_BOOL, word});
                 else if (word == "char")
                     tokens.push_back(Token{T_CHAR, word});
-                else if (word == "agar") 
-                    tokens.push_back(Token{T_AGAR, word});
+                else if (word == "if")
+                    tokens.push_back(Token{T_IF, word});
                 else if (word == "else")
                     tokens.push_back(Token{T_ELSE, word});
                 else if (word == "return")
@@ -244,9 +347,12 @@ private:
     vector<Token> tokens;
     size_t pos;
     Lexer &lexer;
+    SymbolTable symbolTable;
+    int currentScopeLevel;
 
 public:
-    Parser(const vector<Token> &tokens, Lexer &lexer) : tokens(tokens), pos(0), lexer(lexer) {}
+    Parser(const vector<Token> &tokens, Lexer &lexer)
+        : tokens(tokens), pos(0), lexer(lexer), currentScopeLevel(0) {}
 
     void parseProgram()
     {
@@ -268,7 +374,7 @@ public:
         {
             parseAssignment();
         }
-        else if (tokens[pos].type == T_AGAR) 
+        else if (tokens[pos].type == T_IF)
         {
             parseIfStatement();
         }
@@ -303,27 +409,44 @@ public:
 
     void parseDeclaration()
     {
-    
-        if (tokens[pos].type == T_INT || tokens[pos].type == T_FLOAT || tokens[pos].type == T_DOUBLE ||
-            tokens[pos].type == T_STRING || tokens[pos].type == T_CHAR || tokens[pos].type == T_BOOL)
+        TokenType varType = tokens[pos].type;
+        pos++;
+
+        if (tokens[pos].type == T_ID)
         {
+            string varName = tokens[pos].value;
+            symbolTable.insert(varName, varType, currentScopeLevel);
             pos++;
+            expect(T_SEMICOLON);
         }
-        expect(T_ID);
-        expect(T_SEMICOLON);
+        else
+        {
+            cout << "Syntax error: expected identifier after type at line " << lexer.getLineNumber() << endl;
+            exit(1);
+        }
     }
 
     void parseAssignment()
     {
-        expect(T_ID);
+        string varName = tokens[pos].value;
+
+        if (!symbolTable.lookup(varName))
+        {
+            cout << "Error: Variable '" << varName << "' not declared at line " << lexer.getLineNumber() << endl;
+            exit(1);
+        }
+
+        pos++;
         expect(T_ASSIGN);
         parseExpression();
         expect(T_SEMICOLON);
+
+        symbolTable.markInitialized(varName);
     }
 
     void parseIfStatement()
     {
-        expect(T_AGAR); 
+        expect(T_IF);
         expect(T_LPAREN);
         parseExpression();
         expect(T_RPAREN);
@@ -418,11 +541,15 @@ public:
             exit(1);
         }
     }
+    SymbolTable &getSymbolTable()
+    {
+        return symbolTable;
+    }
 };
 
 int main(int argc, char *argv[])
 {
-    
+
     if (argc != 2)
     {
         cout << "Usage: " << argv[0] << " <source-file>" << endl;
@@ -443,6 +570,8 @@ int main(int argc, char *argv[])
 
     Parser parser(tokens, lexer);
     parser.parseProgram();
+
+    parser.getSymbolTable().printTable();
 
     return 0;
 }
